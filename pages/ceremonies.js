322f5841b7fddea33f6ca8014abc3c6deb26fcde
Ayush Tyagi <ayush.tyagi145@gmail.com>
2024-02-02 02:16:49 +0530

    photo added

--- pages/ceremonies.js
+++ pages/ceremonies.js
@@ -62,6 +62,11 @@ const Ceremonies = () => {
             itemType: "image",
             imgUrl: "/ceremonies/ceremonies11.jpeg"
         },
+        {
+            id: 11,
+            itemType: "image",
+            imgUrl: "/ceremonies/ceremonies12.jpg"
+        },
     ]
 
     return (
